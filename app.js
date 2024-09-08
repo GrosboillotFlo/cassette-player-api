eec5fdcad84a67a12c8927eaabb8946749ba90eb
Florian <grosboillotflo@aol.com>
2024-09-08 21:51:55 +0200

    update port for online deployment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import cassettesCollection from "./cassettesCollections.json" assert { type: "js
 
 import express, { json } from "express";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(json());
 
